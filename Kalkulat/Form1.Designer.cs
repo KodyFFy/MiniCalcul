68ddc83c50597928b9cd5e8273295562de466b60
KodyFFy <62069785+KodyFFy@users.noreply.github.com>
2022-06-24 17:02:28 +0300

    Add files via upload

---
+++ Kalkulat/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Kalkulat
+{
+    partial class Калькулятор
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Калькулятор));
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            this.contextMenuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged_2);
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox9
+            // 
+            resources.ApplyResources(this.textBox9, "textBox9");
+            this.textBox9.Name = "textBox9";
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged_1);
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Image = global::Kalkulat.Properties.Resources._2022_05_28_18_53_48;
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Image = global::Kalkulat.Properties.Resources._2022_05_28_18_53_41;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::Kalkulat.Properties.Resources._2022_05_28_18_53_31;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Калькулятор
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Калькулятор";
+            this.Load += new System.EventHandler(this.Калькулятор_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ Kalkulat/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulat
+{
+    public partial class Калькулятор : Form
+    {
+
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile("");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So;
+                Sk = Convert.ToDouble(textBox1.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox3.Text);
+                
+                double answer = mat.Math1(Sk, Skw, So);
+
+
+                textBox7.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox7.Text = "Ошибка";
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So;
+                Sk = Convert.ToDouble(textBox1.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox3.Text);
+
+                double answer = mat.Math1(Sk, Skw, So);
+
+
+                textBox7.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox7.Text = "Ошибка";
+            }
+    }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So;
+                Sk = Convert.ToDouble(textBox1.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox3.Text);
+
+                double answer = mat.Math1(Sk, Skw, So);
+
+
+                textBox7.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox7.Text = "Ошибка";
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox2.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox5_TextChanged_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox5.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged_2(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox5.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox9_TextChanged_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox5.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Maths mat = new Maths();
+                double Sk, Skw, So, i;
+                Sk = Convert.ToDouble(textBox6.Text);
+                Skw = Convert.ToDouble(textBox5.Text);
+                So = Convert.ToDouble(textBox4.Text);
+                i = Convert.ToDouble(textBox9.Text);
+
+                double answer = mat.Math2(Sk, Skw, So, i);
+
+
+                textBox8.Text = answer.ToString();
+            }
+            catch
+            {
+                textBox8.Text = "Ошибка";
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Калькулятор_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    public class Maths
+    {
+        public double Math1(double Sk, double Skw, double So)
+        {
+            double answ = (Sk - Skw) / So;
+            return answ;
+        }
+        public double Math2(double Sk, double Skw, double So, double i)
+        {
+            double SKi = Math.Pow(Skw, i);
+            double answ = (Sk - SKi) / So;
+            return answ;
+        }
+
+    }
